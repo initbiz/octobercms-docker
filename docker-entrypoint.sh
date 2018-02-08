830102ba6a8c99e7eb533a45085628c5a689df7c
Tomasz Strojny <tomasz@init.biz>
2018-02-08 19:09:59 +0100

    Another try

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-curl -fsSL https://raw.githubusercontent.com/Dragontek/octobercms/master/docker-entrypoint.sh > /tmp/dragontekentrypoint.sh
-/bin/bash /tmp/dragontekentrypoint.sh "apache2"
-
 #UID set for volume permissions
 if [ -z "$WWW_DATA_UID" ]; then
    WWW_DATA_UID='33'
@@ -12,4 +9,5 @@ usermod -u $WWW_DATA_UID www-data
 
 chown -R www-data:www-data ~www-data
 
-exec "$@"
+curl -fsSL https://raw.githubusercontent.com/Dragontek/octobercms/master/docker-entrypoint.sh > /tmp/dragontekentrypoint.sh
+/bin/bash /tmp/dragontekentrypoint.sh "apache2"
