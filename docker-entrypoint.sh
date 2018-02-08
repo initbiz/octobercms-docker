ba4c6407ff396f22ee73663f3522d3d0da555b47
Tomasz Strojny <tomasz@init.biz>
2018-02-08 17:59:15 +0100

    Run inherited entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+curl -fsSL https://raw.githubusercontent.com/Dragontek/octobercms/master/docker-entrypoint.sh > /tmp/dragontekentrypoint.sh
+source dragontekentrypoint.sh
+
 #UID set for volume permissions
 if [ -z "$WWW_DATA_UID" ]; then
    WWW_DATA_UID='33'
--- docker-shell.sh
+++ docker-shell.sh
@@ -19,6 +19,6 @@ fi
 name_suffix="_web_1"
 container_name=$project_name$name_suffix
 
-shell=`docker exec -i -t $container_name echo $SHELL`
-docker exec -i -t $container_name $(echo "$shell" | tr -d '\r')
-
+#shell=`docker exec -i -t $container_name echo $SHELL`
+#docker exec -i -t $container_name $(echo "$shell" | tr -d '\r')
+docker exec -i -t $container_name /bin/bash
